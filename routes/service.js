4240847d67af15090ace446881630724c79cfb3c
Bobby Jr Corpus <bobby@corpus.ph>
2019-04-06 23:55:07 +0800

    modified service

--- routes/service.js
+++ routes/service.js
@@ -3,44 +3,9 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
  
-sendFaultString = function(res){
-      console.log("No such method");
-      fs.readFile('data/fault.xml', 'utf8', function(err, contents) {
-        console.log(contents);
-        res.set('Content-Type', 'text/xml; charset=ISO-8859-1');
-        res.send(contents);
-      });
-}
-
-
 router.post('/', function(req, res, next) {
-  var soap_action = req.headers['soapaction'];
-  console.log(req.headers);
-  if(soap_action){ 
-    if(soap_action.indexOf('RegularRecharge') > -1){
-      console.log('Using regularcharge');
-      fs.readFile('data/reg_charge_resp.xml', 'utf8', function(err, contents) {
-        console.log(contents);
-        res.set('Content-Type', 'text/xml; charset=ISO-8859-1');
-        res.send(contents);
-      });
-    }  else if (soap_action.indexOf('GetSubID') > -1){
-
-      fs.readFile('data/subid_resp.xml', 'utf8', function(err, contents) {
-        console.log(contents);
-        res.set('Content-Type', 'text/xml; charset=ISO-8859-1');
-        res.send(contents);
-      });
-
-    } else {
-      sendFaultString(res);
-    }
-  } else {
-    //read fault string  
-    sendFaultString(res);
-  }
-  console.log(soap_action);
- 
+  console.log(req.body);
+  res.status(200).end();
 });
 
 module.exports = router;
