a1c66938ebf495dba280008f6fd37526e6375c9e
Bobby Jr Corpus <bobby@corpus.ph>
2019-04-07 10:02:48 +0800

    Now sends sms using twilio

--- routes/service.js
+++ routes/service.js
@@ -2,10 +2,39 @@ child_process = require('child_process');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
- 
+var twilio = require('twilio');
+
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
+var sendingNumber = process.env.TWILIO_NUMBER;
+var sendToNumbers=process.env.SEND_TO_NUMBERS;
+
+function valid(){
+  return accountSid && authToken && sendingNumber && sendToNumbers;
+}
+
 router.post('/', function(req, res, next) {
-  console.log(req.body);
+  if(valid()){
+    var twilio = require('twilio');
+    
+    var client = new twilio(accountSid, authToken);
+    
+    console.log("Sending SMS");
+    
+    client.messages.create({
+        body: 'Hello from Node',
+        to: sendToNumbers,   // Text this number
+        from: sendingNumber  // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+  
+    console.log("Done calling send");
+  } else {
+    console.log("Invalid inputs");
+  }
+ 
   res.status(200).end();
+
 });
 
 module.exports = router;
