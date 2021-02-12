55d49cb0b6d5159dcf04bffa37c8a986158870e9
corpbob <bobby.corpus@icloud.com>
2021-02-12 15:49:42 +0800

    Add custom message

--- routes/alertmanager.js
+++ routes/alertmanager.js
@@ -3,11 +3,54 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+var towerhost = process.env.TOWER_HOST;
 
 router.post('/', function(req, res, next) {
   var content = req.body;
   console.log(content);
 
+  payload =  
+  { 
+    "status": "status",
+    "labels": {
+      "alertname": "Workflow",
+      "service": "awx",
+      "severity": "info",
+      "instance": "awx",
+      "status": "status",
+      "team": "gyro-success"
+    },
+    "annotations": { 
+      "description": "description"
+    }
+  };
+
+  if(null != content){
+    body = content.body;
+    if(null != body){
+      if(body.includes("approval")){
+        if(body.includes("review")){
+          payload.status = "review";
+          payload.labels.status = "review";
+        } else if (body.includes("approved")) {
+          payload.status="approved";
+          payload.labels.status = "approved";
+        } else if (body.includes("denied")){
+          payload.status="denied";
+          payload.labels.status = "denied";
+        }
+        payload.annotations.description= body.replace("towerhost", towerhost);
+      }
+
+      if(body.includes("Workflow job summary")){
+        payload.status = content.status;
+        payload.labels.status = content.status;
+        payload.labels.alertname = content.name;
+        payload.annotations.description = content.created_by + " : " + body;
+      }
+    }
+    console.log(payload);
+  }
   res.status(200).end();
 
 });
